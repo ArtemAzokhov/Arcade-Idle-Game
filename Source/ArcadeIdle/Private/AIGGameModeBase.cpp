5050c3baada9ecc91ab8bde0715aedb1adccfb5c
Artem Azokhov <azokhov@gmail.com>
2023-10-24 12:41:37 +0400

    Project setup

---
+++ Source/ArcadeIdle/Private/AIGGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Arcade Idle Game. All Rights Reserved.
+
+
+#include "AIGGameModeBase.h"
+
---
+++ Source/ArcadeIdle/Public/AIGGameModeBase.h
@@ -0,0 +1,17 @@
+// Arcade Idle Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADEIDLE_API AAIGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
